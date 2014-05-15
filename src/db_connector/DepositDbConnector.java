d9dafebdda7f7a65adfdfea85eba224cafe67859
Ariel Voskov <Einstine1984@hotmail.com>
2014-05-15 20:08:17 +0300

    Added methods to DepositDbConnector

--- src/db_connector/DepositDbConnector.java
+++ src/db_connector/DepositDbConnector.java
@@ -1,22 +1,23 @@
 package db_connector;
 
+import Managers.DepositManager;
 import classes.Deposit;
+import classes.DepositType;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.sql.Statement;
+import java.util.Date;
 import java.util.logging.Level;
 
-/**
- * Created by Ariel Voskov on 4/30/2014.
- */
-public class DepositDbConnector extends DbConnector {
+public class DepositDbConnector extends DbConnector implements DepositManager{
 
     public DepositDbConnector() {
         connectToDb();
     }
 
-    public void addDeposit(Deposit dep) {
+    public void createDeposit(Deposit dep) {
         try {
 
             String sqlStatement = "INSERT INTO Deposits VALUES(" + dep.getDeposit_id() + ", " + dep.getClient_id() + ", " + dep.getBalance() + ", '" + dep.getType() + "', " + dep.getEstimated_balance() + ", '" + dep.getOpening_date() + "', '" + dep.getClosing_date() + "')";
@@ -32,21 +33,43 @@ public class DepositDbConnector extends DbConnector {
         }
     }
 
-    public void addClient(long client_id, String client_name, String password, String type, String address, String email, String phone, String comment) {
+    @Override
+    public void createNewDeposit(long deposit_id, long client_id, double balance, DepositType type, long estimated_balance, String opening_date, String closing_date) {
         try {
-
-            String smstStr = "INSERT INTO Clients VALUES(" + client_id + ", '" + client_name + "', '" + password + "', '" + type + "', '" + address + "', '" + email + "', '" + phone + "', '" + comment + "')";
+            String smstStr = "INSERT INTO Clients VALUES(" + deposit_id + ", " + client_id + ", " + balance + ", '" + type + "', " + estimated_balance + ", '" + opening_date + "', '" + closing_date + "')";
             stmt.executeUpdate(smstStr);
-            String msg = "Client " + client_name + " was created on DB";
+            String msg = "Deposit" + deposit_id + " with " + balance + " balance was created on DB";
             LOGGER.log(Level.INFO, msg);
-
         } catch (SQLIntegrityConstraintViolationException e) {
-            String msg = "Client " + client_id + " already exists on DB. Client wasn't added";
+            String msg = "Deposit " + deposit_id + " already exists on DB. Deposit wasn't added";
             LOGGER.log(Level.WARNING, msg);
         } catch (SQLException e) {
             LOGGER.log(Level.WARNING, e.getMessage());
             e.printStackTrace();
         }
+    }
 
+    public void closeDeposit(long deposit_id) {
+        try{
+            double balance;
+            long client_id = 0;
+            String smstStr = "SELECT * FROM Deposits WHERE deposit_id = \"" + deposit_id + "\"";
+            ResultSet res = stmt.executeQuery(smstStr);
+            AccountDbConnector AC = new AccountDbConnector() ;
+            if (res.next()){
+                balance = res.getDouble(3);
+                client_id = res.getLong(2);
+                AC.depositToAccount();
+                String msg = "deposit " + deposit_id + "was closed. " + balance + " was added to ";
+                LOGGER.log(Level.INFO, msg);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public double preopenDeposit() {
+        return 0;
     }
 }
