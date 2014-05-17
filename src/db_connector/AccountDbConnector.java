8c344cee58c57a32d899aabc7623bb42f920169f
Ariel Voskov <Einstine1984@hotmail.com>
2014-05-17 21:10:55 +0300

    Added beans usage to account_db_connector Fixed a cuople of bugs

--- src/db_connector/AccountDbConnector.java
+++ src/db_connector/AccountDbConnector.java
@@ -9,6 +9,7 @@ import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.logging.Level;
 
 public class AccountDbConnector extends DbConnector implements AccountManager {
+    private String sqlStatement;
     public AccountDbConnector() {
         connectToDb();
     }
@@ -16,7 +17,7 @@ public class AccountDbConnector extends DbConnector implements AccountManager {
     public void createAccount(Account acc) {
         try {
 
-            String sqlStatement = "INSERT INTO Accounts VALUES(" + acc.getAccount_id() + ", " + acc.getClient_id() + ", " + acc.getBalance() + ", " + acc.getCredit_limit() + ", '" + acc.getComment() + "')";
+            sqlStatement = "INSERT INTO Accounts VALUES(" + acc.getAccount_id() + ", " + acc.getClient_id() + ", " + acc.getBalance() + ", " + acc.getCredit_limit() + ", '" + acc.getComment() + "')";
             stmt.executeUpdate(sqlStatement);
             String msg = "Account " + acc.getAccount_id() + " was created on DB";
             LOGGER.log(Level.INFO, msg);
@@ -28,10 +29,60 @@ public class AccountDbConnector extends DbConnector implements AccountManager {
         }
     }
 
+    @Override
+    public void deleteAccount(Account account) {
+        try {
+            sqlStatement = "DELETE FROM Accounts WHERE account_id=" + account.getAccount_id();
+            stmt.executeUpdate(sqlStatement);
+            String msg = "Account " + account.getAccount_id() + " was deleted from DB";
+            LOGGER.log(Level.INFO, msg);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void withdrawFromAccount(Account account, double withdraw_amount) {
+        sqlStatement = "Select balance, credit_limit FROM Accounts WHERE account_id=" + account.getAccount_id();
+        try {
+            ResultSet res = stmt.executeQuery(sqlStatement);
+            double balance, cred_lim = 0, new_balance = 0;
+            if (res.next()) {
+                balance = res.getDouble(1);
+                cred_lim = res.getDouble(2);
+                new_balance = balance - withdraw_amount;
+            }
+            if (new_balance >= cred_lim) {
+                sqlStatement = "UPDATE Accounts SET balance=" + new_balance;
+                stmt.executeUpdate(sqlStatement);
+                String msg = withdraw_amount + " was withdrawn from account " + account.getAccount_id() + ". The new balance is " + new_balance;
+                LOGGER.log(Level.INFO, msg);
+            } else {
+                String msg = "Your credit limit isn't large enough to withdraw " + withdraw_amount;
+                LOGGER.log(Level.INFO, msg);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void depositToAccount(Account account, double amount) {
+        try {
+            sqlStatement = "DELETE FROM Accounts WHERE account_id=" + account.getAccount_id();
+            stmt.executeUpdate(sqlStatement);
+            String msg = "Account " + account.getAccount_id() + " was deleted from DB";
+            LOGGER.log(Level.INFO, msg);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
     public void createAccount(long id, long client_id, double balance, double credit_limit, String comment) {
         try {
-
-            String sqlStatement = "INSERT INTO Accounts VALUES(" + id + ", " + client_id + ", " + balance + ", " + credit_limit + ", '" + comment + "')";
+            sqlStatement = "INSERT INTO Accounts VALUES(" + id + ", " + client_id + ", " + balance + ", " + credit_limit + ", '" + comment + "')";
             stmt.executeUpdate(sqlStatement);
             String msg = "Account " + id + " was created on DB";
             LOGGER.log(Level.INFO, msg);
@@ -46,18 +97,17 @@ public class AccountDbConnector extends DbConnector implements AccountManager {
 
     public void deleteAccount(long id) {
         try {
-            String sqlStatement = "DELETE FROM Accounts WHERE account_id=" + id;
+            sqlStatement = "DELETE FROM Accounts WHERE account_id=" + id;
             stmt.executeUpdate(sqlStatement);
             String msg = "Account " + id + " was deleted from DB";
             LOGGER.log(Level.INFO, msg);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
     }
 
     public void withdrawFromAccount(long account_id, double withdraw_amount) {
-        String sqlStatement = "Select balance, credit_limit FROM Accounts";
+        sqlStatement = "Select balance, credit_limit FROM Accounts";
         try {
             ResultSet res = stmt.executeQuery(sqlStatement);
             double balance, cred_lim = 0, new_balance = 0;
@@ -81,7 +131,7 @@ public class AccountDbConnector extends DbConnector implements AccountManager {
     }
 
     public void depositToAccount(long account_id, double deposit_amount) {
-        String sqlStatement = "SELECT balance FROM Accounts";
+        sqlStatement = "SELECT balance FROM Accounts";
         try {
             double balance, new_balance = 0;
             ResultSet res = stmt.executeQuery(sqlStatement);
@@ -100,7 +150,20 @@ public class AccountDbConnector extends DbConnector implements AccountManager {
     }
 
     public void checkBalance(long account_id) {
-        String sqlStatement = "SELECT balance FROM Accounts";
+        sqlStatement = "SELECT balance FROM Accounts WHERE account_id" + account_id;
+        try {
+            ResultSet res = stmt.executeQuery(sqlStatement);
+            if (res.next()) {
+                double balance = res.getDouble(1);
+                LOGGER.log(Level.INFO, String.valueOf(balance));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void checkBalance(Account account) {
+        sqlStatement = "SELECT balance FROM Accounts WHERE account_id=" + account.getAccount_id();
         try {
             ResultSet res = stmt.executeQuery(sqlStatement);
             if (res.next()) {
