2d52c1ad8e77ad2afd29885ee5316b2d708a0f90
Ariel Voskov <Einstine1984@hotmail.com>
2014-05-21 08:53:33 +0300

    Working on preopening deposits

--- src/Managers/DepositManager.java
+++ src/Managers/DepositManager.java
@@ -1,10 +1,11 @@
 package Managers;
 
+import classes.Deposit;
 import classes.DepositType;
 
 public interface DepositManager {
     void createNewDeposit(long deposit_id, long client_id, double balance, DepositType type, long estimated_balance, String opening_date, String closing_date);
-    void closeDeposit();
-    double preopenDeposit();
-
+    void createNewDeposit(Deposit deposit);
+    void closeDeposit(long deposit_id);
+    void closeDeposit(Deposit deposit);
 }
