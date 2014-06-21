d14f90552caad60c280ae839fc69856c08557a27
Ariel Voskov <Einstine1984@hotmail.com>
2014-06-21 19:42:49 +0300

    Testing clientActions

--- src/test/ClientActionTest.java
+++ src/test/ClientActionTest.java
@@ -1,9 +1,14 @@
-package main.services;
+package test;
 
 import init.InitiateDB;
 import main.AccountType;
+import main.db_access_layer.managers.AccountManager;
+import main.db_access_layer.managers.ClientManager;
+import main.db_access_layer.managers.impl.AccountManagerImpl;
+import main.db_access_layer.managers.impl.ClientManagerImpl;
 import main.model.Account;
 import main.model.Client;
+import main.services.ClientAction;
 import main.services.impl.ClientActionImpl;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,71 +19,84 @@ public class ClientActionTest {
     Client testClient;
     Account testAccount;
     ClientAction ca;
+    ClientManager cm;
+    AccountManager am;
+
     @Before
     public void setUp() throws Exception {
         InitiateDB.restartDb();
         testClient = new Client(1, "test Client", "wertyuio", AccountType.REGULAR, "qwertyui fgh jkl", "wertyui@lkjhgfd.com", "876543", "Test client");
         testAccount = new Account(1, 1, 1000, 10000, "Test Account");
         ca = new ClientActionImpl();
-
+        cm = new ClientManagerImpl();
+        am = new AccountManagerImpl();
+        cm.createClient(testClient);
+        am.createAccount(testAccount);
     }
 
     @Test
     public void testWithdrawFromAccount() throws Exception {
         ca.withdrawFromAccount(testAccount, 100);
+        Account dbAccount = am.findAccount(1);
+        assertEquals(899.5, dbAccount.getBalance(), 0.0001);
     }
 
     @Test
     public void testWithdrawFromAccount1() throws Exception {
         ca.withdrawFromAccount(1, 100);
+        Account dbAccount = am.findAccount(1);
+        assertEquals(899.5, dbAccount.getBalance(), 0.0001);
+
     }
 
     @Test
     public void testWithdrawFromAccount2() throws Exception {
         ca.withdrawFromAccount(testClient, 100);
+        Account dbAccount = am.findAccount(1);
+        assertEquals(899.5, dbAccount.getBalance(), 0.0001);
     }
 
-    @Test
+    //@Test
     public void testDepositToAccount() throws Exception {
 
     }
 
-    @Test
+    //@Test
     public void testDepositToAccount1() throws Exception {
 
     }
 
-    @Test
+    //@Test
     public void testDepositToAccount2() throws Exception {
 
     }
 
-    @Test
+    //@Test
     public void testCreateNewDeposit() throws Exception {
 
     }
 
-    @Test
+    //@Test
     public void testCreateNewDeposit1() throws Exception {
 
     }
 
-    @Test
+    //@Test
     public void testPreOpenDeposit() throws Exception {
 
     }
 
-    @Test
+    //@Test
     public void testUpdateClientDetails() throws Exception {
 
     }
 
-    @Test
+    //@Test
     public void testViewClientDeposits() throws Exception {
 
     }
 
-    @Test
+    //@Test
     public void testViewClientActivities() throws Exception {
 
     }
