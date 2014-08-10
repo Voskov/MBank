59fa93d0ff4e80ae3848ed82e694f398803b65da
Voskov <Einstine1984@hotmail.com>
2014-08-10 22:09:07 +0300

    Change initiateDB to initiate

--- src/init/InitiateDB.java
+++ src/init/InitiateDB.java
@@ -7,7 +7,7 @@ import java.sql.Statement;
 
 public class InitiateDB {
     public static void main(String[] args) {
-        restartDb();
+        createDb();
     }
 
     public static void restartDb(){
